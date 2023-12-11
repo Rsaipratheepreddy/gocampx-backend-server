f48537f7a95376b5c469f6955dae99d86ce8d319
R Sai Pratheep Reddy <saipratheepreddy2818@gmail.com>
2023-12-11 21:08:59 +0530

    add required repos

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+import { VendorModule } from './vendor/vendor.module';
+import { OrderModule } from './order/order.module';
+import { PaymentsModule } from './payments/payments.module';
+import { WalletModule } from './wallet/wallet.module';
+import { ServicesModule } from './services/services.module';
 
 @Module({
-  imports: [],
+  imports: [UserModule, VendorModule, OrderModule, PaymentsModule, WalletModule, ServicesModule],
   controllers: [AppController],
   providers: [AppService],
 })
